b439213cf31b554e41ceda7b6634bc29e5a32777
Artem <kluv.rus@gmail.com>
2019-11-08 13:07:15 +0300

    Updates

---
+++ precache-manifest.863d94651de2e60a4cd854d2e406b3ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08faa84c22e488e2dcc11c3595ed89d9",
+    "url": "/maxpfrontend-tz-1-v2/index.html"
+  },
+  {
+    "revision": "595a14221e222b10f3be",
+    "url": "/maxpfrontend-tz-1-v2/static/css/main.a7120bad.chunk.css"
+  },
+  {
+    "revision": "91b61c08616fcb3159fe",
+    "url": "/maxpfrontend-tz-1-v2/static/js/2.5a5e8581.chunk.js"
+  },
+  {
+    "revision": "595a14221e222b10f3be",
+    "url": "/maxpfrontend-tz-1-v2/static/js/main.68377765.chunk.js"
+  },
+  {
+    "revision": "969c52b23d49054845e2",
+    "url": "/maxpfrontend-tz-1-v2/static/js/runtime-main.381e6fd5.js"
+  }
+]);
